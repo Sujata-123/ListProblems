a34f8331a29a2b4c8872ad807e4f04ceb47e72d7
Sujata-123 <sujata672singh@gmail.com>
2021-06-10 16:22:46 +0530

    1 file modified

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 //list problems
 // Write a function that returns the largest element in a list.
-// let array1 = [5, 12, 8, 130, 500];
-// let max = array1[0]; assuming as max is at index zero
+let array1 = [5, 12, 8, 130, 500];
+let max = array1[0]; //assuming as max is at index zero
 
-// for (let i = 1; i < array1.length; i++) {
-//   if (max < array1[i]) {
-//     max = array1[i];
-//   }
-// }
-// document.write(`Max element is ${max}`);
+for (let i = 1; i < array1.length; i++) {
+  if (max < array1[i]) {
+    max = array1[i];
+  }
+}
+document.write(`Max element is ${max}`);
 
 // Write function that reverses a list, preferably in place.
 
-// let no=543289;
-// let rem;
-// let rev=0;
-// while(no!=0)
-// {
-// rem=parseInt(no%10);
-// rev=parseInt(rev*10+rem);
-// no=parseInt(no/10);
-// }
-// document.write(rev);
+let no = 543289;
+let rem;
+let rev = 0;
+while (no != 0) {
+  rem = parseInt(no % 10);
+  rev = parseInt(rev * 10 + rem);
+  no = parseInt(no / 10);
+}
+document.write(rev);
