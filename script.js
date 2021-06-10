7aecc0af6edebcc15cdfa2eb435b970f1750749f
Sujata-123 <sujata672singh@gmail.com>
2021-06-10 16:21:30 +0530

    1 file modified

--- script.js
+++ script.js
@@ -11,12 +11,14 @@
 // document.write(`Max element is ${max}`);
 
 // Write function that reverses a list, preferably in place.
-let no = 543289;
-let rem;
-let rev = 0;
-while (no != 0) {
-  rem = no % 10;
-  rev = rev * 10 + rem;
-  no = no / 10;
-}
-document.write(rev);
+
+// let no=543289;
+// let rem;
+// let rev=0;
+// while(no!=0)
+// {
+// rem=parseInt(no%10);
+// rev=parseInt(rev*10+rem);
+// no=parseInt(no/10);
+// }
+// document.write(rev);
